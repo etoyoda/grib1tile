a4c7b15ee37898826ef9a8f2b63e2fee6edbcd8b
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-07-30 00:02:00 +0000

    now we can scan magic number "GRIB"

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 OBJS=gribscan.o
 LIBS=
-CFLAGS=
+LFLAGS= -g
+CFLAGS= -g --pedantic
+CC=cc
 
 .SUFFIXES:
 .SUFFIXES: .c .o
 
-grib1scan:
-	$(CC) -o grib1scan $(OBJS) $(LIBS)
+grib1scan: $(OBJS)
+	$(CC) $(LFLAGS) -o grib1scan $(OBJS) $(LIBS)
 
 .c.o:
 	$(CC) -c $(CFLAGS) $<
--- gribscan.c
+++ gribscan.c
@@ -1,7 +1,86 @@
 #include <stdio.h>
 
-  int
-main(int argc, char **argv)
+  unsigned
+gdecode(FILE *fp)
+{
+  unsigned r;
+  r = 1;
+  return r;
+}
+
+  unsigned
+scandata(const char *fnam)
+{
+  FILE *fp;
+  long lpos = 0;
+  int c;
+  int state = 0;
+  unsigned r = 0;
+  fp = fopen(fnam, "rb");
+  if (fp == NULL) { goto error; }
+  /* automaton to find the magic number "GRIB */
+  while ((c = getc(fp)) != EOF) {
+    switch (state) {
+    default:
+    case 0:
+      if (c == 'G') { state = c; lpos = ftell(fp); } else { state = 0; }
+      break;
+    case 'G':
+      if (c == 'R') { state = c; } else { state = 0; }
+      break;
+    case 'R':
+      if (c == 'I') { state = c; } else { state = 0; }
+      break;
+    case 'I':
+      if (c == 'B') {
+	r = gdecode(fp);
+	if (r != 0) {
+	  fprintf(stderr, "%s:%lu: GRIB1 decode %u\n", fnam, lpos, r);
+	}
+	if (r & ~1) goto klose;
+      }
+      state = 0;
+      break;
+    }
+  }
+klose:
+  if (fclose(fp) != 0) { goto error; }
+  return r;
+error:
+  perror(fnam);
+  return 2;
+}
+
+  unsigned
+scanconfig(const char *fnam)
 {
   return 0;
+}
+
+  int
+main(int argc, const char **argv)
+{
+  unsigned r;
+  unsigned nconfig = 0, ndatafile = 0;
+  const char **args;
+  for (args = argv + 1; *args; args++) {
+    if (nconfig == 0) {
+      r = scanconfig(*args);
+      if (r != 0) goto err;
+      nconfig = 1;
+    } else {
+      /* 0: okay, 1: warning, other bits: fatal */
+      r = scandata(*args);
+      if (r & ~1) goto err;
+      if (r == 0) ndatafile++;
+    }
+  }
+  if (ndatafile == 0) {
+    fprintf(stderr, "usage: %s config data ...\n", argv[0]);
+    r = 2;
+  }
+  return r;
+err:
+  fprintf(stderr, "%s: exit(%u)\n", argv[0], r);
+  return r;
 }
