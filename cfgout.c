4e0f224272a750c6928f54b6d1ad625d5645f9f8
TOYODA Eizi <toyoda.eizi@gmail.com>
2020-01-29 11:59:38 +0000

    growing

--- cfgout.c
+++ cfgout.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <time.h>
 
 #include "gribscan.h"
 
+struct cfgout_t {
+  time_t rtime;
+  unsigned ft;
+  int level;
+  int elem;
+};
+
+static struct cfgout_t *cfg = NULL;
+
   int
-new_cfgout(unsigned size)
+new_cfgout(unsigned siz)
 {
+  cfg = calloc(siz, sizeof(struct cfgout_t));
+  if (cfg == NULL) {
+    return ENOMEM;
+  }
   return 0;
 }
 
