9df60f09a03fff094a610ea87d2bad5fd3e1e2fd
TOYODA Eizi <toyoda.eizi@gmail.com>
2020-05-31 22:48:15 +0900

    growing test - とりあえず入力格子位置をまともに算出できているかチェックできた

--- xweight.c
+++ xweight.c
@@ -2,8 +2,35 @@
 #include <stdlib.h>
 
   void
+accregular(unsigned nx, unsigned ny, float oary[],
+  double lon1, double lon2, unsigned nlon,
+  double lat1, double lat2, unsigned nlat)
+{
+  unsigned ilon, ilat;
+  for (ilat = 0; ilat < nlat; ilat++) {
+    double lat = lat1 + ((lat2 - lat1) * ilat) / (nlat - 1);
+    for (ilon = 0; ilon < nlon; ilon++) {
+      double lon = lon1 + ((lon2 - lon1) * ilon) / (nlon - 1);
+      printf("%3u %3u %8.2f %8.2f\n", ilat, ilon, lat, lon);
+    }
+  }
+}
+
+  void
 accweight(unsigned nx, unsigned ny, float oary[], int igrid)
 {
+  switch (igrid) {
+  case 37:
+  break;
+  case 44:
+  break;
+  case 255:
+    accregular(nx, ny, oary, 60.0, 200.0, 113u, 60.0, -20.0, 65u);
+  break;
+  default:
+    fprintf(stderr, "invalid igrid=%d\n", igrid);
+  break;
+  }
 }
 
   int
