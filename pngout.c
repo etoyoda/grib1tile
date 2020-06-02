01b9a353493e08ebeebfd1c92bddd53298c27c1f
TOYODA Eizi <toyoda.eizi@gmail.com>
2020-06-02 00:16:01 +0000

    minimalizing

--- pngout.c
+++ pngout.c
@@ -8,21 +8,17 @@
 
 #define EPNGFATAL ENETDOWN
 
-struct outparams {
-  unsigned width;
-  unsigned height;
-  const char *filename;
-};
-
   int
-writeimg(const struct outparams *op, png_bytep *ovector)
+writepng(unsigned width, unsigned height, png_bytep *ovector,
+  const char *filename)
 {
-  FILE *fp = fopen(op->filename, "wb");
   const int NTEXT = 1;
   png_text txtbuf[NTEXT];
+  png_structp png;
+  FILE *fp;
+  fp = fopen(filename, "wb");
   if (!fp) { return EOF; }
-  png_structp png = png_create_write_struct(
-    PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
   if (!fp) { errno = EPNGFATAL; return EOF; }
   png_infop info = png_create_info_struct(png);
   if (!info) { errno = EPNGFATAL; return EOF; }
@@ -33,7 +29,7 @@ writeimg(const struct outparams *op, png_bytep *ovector)
   txtbuf[0].text_length = strlen(txtbuf[0].text);
   png_set_text(png, info, txtbuf, NTEXT);
   png_init_io(png, fp);
-  png_set_IHDR(png, info, op->width, op->height, 8, PNG_COLOR_TYPE_RGBA,
+  png_set_IHDR(png, info, width, height, 8, PNG_COLOR_TYPE_RGBA,
     PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
   png_write_info(png, info);
   png_write_image(png, ovector);
